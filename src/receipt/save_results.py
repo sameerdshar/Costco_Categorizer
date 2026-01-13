b12d18407a6c19e7eb06c149d7b7d984d2de53af
Sameer <sameer.dc.sharma@gmail.com>
2026-01-13 14:44:32 -0600

    Add logic to export excel file with formulae

--- src/receipt/save_results.py
+++ src/receipt/save_results.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from openpyxl import load_workbook
 
 
 def dict_to_excel(data: dict, output_path: str):
@@ -9,3 +10,14 @@ def dict_to_excel(data: dict, output_path: str):
         output_path (str): Path to the output Excel file.'''
     df = pd.DataFrame(data, columns=data[0].keys())
     df.to_excel(output_path, index=False)
+
+    # Load workbook and worksheet
+    wb = load_workbook(output_path)
+    ws = wb.active
+
+    ws["G1"] = "Essentials Total"
+    ws["G2"] = '=SUMIF($E$2:$E$1000, "essential", $D$2:$D$1000)'
+    ws["H1"] = "Non-Essentials Total"
+    ws["H2"] = '=SUMIF($E$2:$E$1000, "", $D$2:$D$1000)'
+
+    wb.save(output_path)
